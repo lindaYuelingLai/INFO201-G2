cee483b16f1e846e83c27158f6ddaf7f6313ffa7
Nikki Mogadas <nikkimogadas@Nikkis-MacBook-Pro.local>
2018-12-01 13:11:25 -0800

    added side panel buttons

--- scripts/ui.R
+++ scripts/ui.R
@@ -7,11 +7,18 @@ ui <- fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      
+      radioButtons("race", h3("Select a race:"),
+                   c("All" = "all","White" = "white","Black" = "black","Asian"="asian",
+                     "Native American"="native","Hispanic"="hispanic"),
+      selectInput("factors", h3("Other factors describing victim:"), 
+                  c("Armed"="arm","Signs of Mental Illness"="mental","Threat Level"="threat","Fleeing"="flee"), 
+                  selected = NULL, multiple = FALSE,
+                  selectize = TRUE, width = NULL, size = NULL)
+      )
     )
   ),
   mainPanel(
-    plotOutput("")
+  plotOutput("distPlot")
   )
 )
 
