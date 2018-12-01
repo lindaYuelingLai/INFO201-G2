cd30d64458b2130968e791580d604fe294a2cca1
lindaYuelingLai <yl273@uw.edu>
2018-12-01 13:08:41 -0800

    added command for subsets

--- scripts/server.R
+++ scripts/server.R
@@ -1,9 +1,13 @@
 library(shiny)
 library(dplyr)
 library(ggplot2)
+shooting_data <- read.csv("shootings_data.csv", stringsAsFactors = FALSE)
 
 server <- function(input, output) {
-  shooting_data <- read.csv("shootings_data.csv", stringsAsFactors = FALSE)
+  # get the subset for the selected variable
+  race_data <- reactive({
+    shooting_data %>% group_by(race) %>% summarise(n = n()) %>% arrange(desc(n))
+  })
 }
 
 shinyServer(server)
