9d45b434b777306f9889152d778de7a84e899741
Nikki Mogadas <nikkimogadas@Nikkis-MacBook-Pro.local>
2018-12-01 13:48:17 -0800

    updated ui and server files

--- scripts/ui.R
+++ scripts/ui.R
@@ -9,16 +9,15 @@ ui <- fluidPage(
     sidebarPanel(
       radioButtons("race", h3("Select a race:"),
                    c("All" = "all","White" = "white","Black" = "black","Asian"="asian",
-                     "Native American"="native","Hispanic"="hispanic"),
+                     "Native American"="native","Hispanic"="hispanic")),
       selectInput("factors", h3("Other factors describing victim:"), 
                   c("Armed"="arm","Signs of Mental Illness"="mental","Threat Level"="threat","Fleeing"="flee"), 
                   selected = NULL, multiple = FALSE,
                   selectize = TRUE, width = NULL, size = NULL)
-      )
+      ),
+    mainPanel(
+      plotOutput("statePlot")
     )
-  ),
-  mainPanel(
-  plotOutput("distPlot")
   )
 )
 
