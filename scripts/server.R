bd6664518b9290b4d5cdb897b1ef1ee1fa39b54f
lia kitahata <liarose@Lias-MacBook-Pro.local>
2018-12-01 13:12:13 -0800

    Add function to graph race by state

--- scripts/server.R
+++ scripts/server.R
@@ -4,6 +4,25 @@ library(ggplot2)
 
 server <- function(input, output) {
   shooting_data <- read.csv("shootings_data.csv", stringsAsFactors = FALSE)
+  
+  race_by_state <- function(the_race) {
+    if (the_race == "W") title <- "White"
+    if (the_race == "B") title <- "Black"
+    if (the_race == "H") title <- "Hispanic"
+    if (the_race == "A") title <- "Asian"
+    if (the_race == "N") title <- "Native American"
+    race_data <- filter(shooting_data, race == the_race)
+    race_data <- race_data %>% group_by(state) %>% summarise(n = n()) %>% arrange(desc(n))
+    result_plot <- ggplot(race_data, aes(race_data, x = state, y = n)) +
+      geom_bar(stat="identity", width = 1) +
+      labs(
+        title = paste0("Fatal Shootings by State, ", title),
+        x = "State",
+        y = "Reports"
+      ) +
+      theme(axis.text.x=element_text(size=rel(1), angle=90))
+    return(result_plot)
+  }
 }
 
 shinyServer(server)
